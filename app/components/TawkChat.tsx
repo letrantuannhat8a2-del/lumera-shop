5d603e62206093ac95060590b258cbecc1261fe3
Le Nhat <letrantuannhat8a2@gmail.com>
2026-08-22 14:15:29 +0700

    fix tawk loading production

--- app/components/TawkChat.tsx
+++ app/components/TawkChat.tsx
@@ -9,7 +9,7 @@ export default function TawkChat() {
   const pathname = usePathname();
 
 
-  // Không hiển thị chat trong trang admin
+  // Ẩn chat trong admin
   if (pathname.startsWith("/admin")) {
     return null;
   }
@@ -17,40 +17,9 @@ export default function TawkChat() {
 
   return (
     <Script
-      id="tawk-chat"
+      id="tawk-widget"
+      src="https://embed.tawk.to/6a8934f5bc557a344a5e4987/1k0jvdno4"
       strategy="afterInteractive"
-      dangerouslySetInnerHTML={{
-        __html: `
-        
-        var Tawk_API = Tawk_API || {};
-        var Tawk_LoadStart = new Date();
-
-        (function () {
-
-          var s1 = document.createElement("script"),
-          s0 = document.getElementsByTagName("script")[0];
-
-          s1.async = true;
-
-          s1.src =
-          "https://embed.tawk.to/6a8934f5bc557a344a5e4987/1k0jvdno4";
-
-          s1.charset = "UTF-8";
-
-          s1.setAttribute(
-            "crossorigin",
-            "*"
-          );
-
-          s0.parentNode.insertBefore(
-            s1,
-            s0
-          );
-
-        })();
-
-        `,
-      }}
     />
   );
 }
